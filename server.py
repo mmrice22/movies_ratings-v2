dfaa174f31a7568934ebc2aae8afbdfc05c445f0
Melissa Rice <mmrice22@gmail.com>
2021-03-26 16:23:00 +0000

    final push

--- server.py
+++ server.py
@@ -39,6 +39,39 @@ def show_movie(movie_id):
     return render_template('movie_details.html', movie = movie)
 
 
+@app.route('/users', methods=['POST'])
+def register_user():
+    """Create a new uesr."""
+    new_email = request.form.get('email')
+    new_password = request.form.get('password')
+
+     # if email exists, flash message to say you can't create an account 
+     # if it doens't, create new user flash message telling it created successfully
+
+    user = crud.get_user_by_email(new_email)
+
+    if user:
+        flash("Can't create an account with that email. Try again.")
+    else: 
+        crud.create_user(new_email, new_password)
+        flash('Account created. Please log in.')
+    
+    return redirect('/')
+
+
+@app.route('/login', methods=['POST'])
+def login_user():
+    user_email = request.form.get('email')
+    user_password = request.form.get('password') 
+
+    user_id = crud.user_id_if_match(password)
+    session['Current User'] = user_id
+
+    flash('Logged in!')
+    
+    redirect('/')
+
+
 @app.route('/users')
 def all_users():
     """View all users"""
